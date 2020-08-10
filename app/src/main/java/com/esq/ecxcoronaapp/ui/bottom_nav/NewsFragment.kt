9d7080a7d9b0d1b6c94bad7b10b55f1a481e20af
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-08-10 23:24:31 +0100

    I edited a news Fragment and it's layout

--- app/src/main/java/com/esq/ecxcoronaapp/ui/bottom_nav/NewsFragment.kt
+++ app/src/main/java/com/esq/ecxcoronaapp/ui/bottom_nav/NewsFragment.kt
@@ -6,10 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.esq.ecxcoronaapp.R
+import com.esq.ecxcoronaapp.data.NewsFragmentAdapter
+import kotlinx.android.synthetic.main.news_fragment.*
 
 class NewsFragment : Fragment() {
+
+    private lateinit var adapter: NewsFragmentAdapter
+    private lateinit var recyclerView: RecyclerView
 
     companion object {
         private val TAG = this::class.simpleName
@@ -27,8 +36,19 @@ class NewsFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(NewsViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
+        recyclerView = news_recycler_view
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        viewModel.homeAdapterData.observe(viewLifecycleOwner, Observer {
+            it.let {
+                adapter = NewsFragmentAdapter(activity?.applicationContext!!, it)
+                recyclerView.adapter = adapter
+            }
+        })
+
     }
 
 }
