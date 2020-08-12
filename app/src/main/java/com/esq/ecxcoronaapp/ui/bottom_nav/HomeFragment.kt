2209d596e9ec04374e75e63954334f505f9071ca
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-08-12 12:04:20 +0100

    Log added.

--- app/src/main/java/com/esq/ecxcoronaapp/ui/bottom_nav/HomeFragment.kt
+++ app/src/main/java/com/esq/ecxcoronaapp/ui/bottom_nav/HomeFragment.kt
@@ -1,16 +1,15 @@
 package com.esq.ecxcoronaapp.ui.bottom_nav
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import com.esq.ecxcoronaapp.R
 import com.esq.ecxcoronaapp.data.HomeFragmentAdapter
 import kotlinx.android.synthetic.main.home_fragment.*
@@ -43,11 +42,11 @@ class HomeFragment : Fragment() {
 
         viewModel.homeAdapterData.observe(viewLifecycleOwner, Observer {
             it.let {
+                Log.d(TAG, if(it.isNullOrEmpty()) "Empty List" else "$it")
                 adapter = HomeFragmentAdapter(activity?.applicationContext!!, it)
                 recyclerView.adapter = adapter
-            }
+           }
         })
-
     }
 
 }
