100e7e85916292b69a20733993008920acc289ef
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-08-14 01:21:22 +0100

    New changes made. Databinding removed.

--- app/src/main/java/com/esq/ecxcoronaapp/data/NewsFragmentAdapter.kt
+++ app/src/main/java/com/esq/ecxcoronaapp/data/NewsFragmentAdapter.kt
@@ -2,17 +2,25 @@ package com.esq.ecxcoronaapp.data
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.esq.ecxcoronaapp.R
-import com.esq.ecxcoronaapp.databinding.ContentNewsRecyclerListItemBinding
 import com.esq.ecxcoronaapp.domain.model.NewsListModel
+import kotlinx.android.synthetic.main.content_news_recycler_list_item.view.*
 
-class NewsFragmentAdapter(private val context: Context, private val homeListInfo: List<NewsListModel>) :
+class NewsFragmentAdapter(
+    private val context: Context,
+    private val homeListInfo: List<NewsListModel>
+) :
     RecyclerView.Adapter<NewsFragmentAdapter.MyViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = DataBindingUtil.inflate<ContentNewsRecyclerListItemBinding>(LayoutInflater.from(context), R.layout.content_news_recycler_list_item, parent, false)
+        val view =
+            LayoutInflater.from(context).inflate(
+                R.layout.content_news_recycler_list_item,
+                parent,
+                false
+            )
         return MyViewHolder(view)
     }
 
@@ -25,13 +33,12 @@ class NewsFragmentAdapter(private val context: Context, private val homeListInfo
         holder.setData(homeInfo)
     }
 
-    inner class MyViewHolder(private val binding: ContentNewsRecyclerListItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun setData(newsListModel: NewsListModel?) {
-            newsListModel?.let {
-                with(newsListModel){
-                    binding.imageViewForCard.setImageResource(image)
-                    binding.title.text = news
-                }
+    inner class MyViewHolder(private val item: View) :
+        RecyclerView.ViewHolder(item) {
+        fun setData(newsListModel: NewsListModel) {
+            with(newsListModel) {
+                item.imageViewForCard.setImageDrawable(context.getDrawable(image))
+                item.title.text = context.getString(news)
             }
         }
     }
