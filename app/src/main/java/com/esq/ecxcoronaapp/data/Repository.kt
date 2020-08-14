c10964f7e849b9d23c5b714a5ef17d1e2787fcba
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-08-14 02:59:08 +0100

    Introduced a companion object

--- app/src/main/java/com/esq/ecxcoronaapp/data/Repository.kt
+++ app/src/main/java/com/esq/ecxcoronaapp/data/Repository.kt
@@ -4,36 +4,37 @@ import com.esq.ecxcoronaapp.R
 import com.esq.ecxcoronaapp.domain.model.NewsListModel
 
 class Repository() {
-
-    val newsList = listOf(
-        NewsListModel(
-            R.drawable.news_images1,
-            R.string.dummy_text
-        ),
-        NewsListModel(
-            R.drawable.news_images2,
-            R.string.dummy_text
-        ),
-        NewsListModel(
-            R.drawable.news_images3,
-            R.string.dummy_text
-        ),
-        NewsListModel(
-            R.drawable.news_images4,
-            R.string.dummy_text
-        ),
-        NewsListModel(
-            R.drawable.news_images5,
-            R.string.dummy_text
-        ), NewsListModel(
-            R.drawable.news_images3,
-            R.string.dummy_text
-        ), NewsListModel(
-            R.drawable.news_images3,
-            R.string.dummy_text
-        ), NewsListModel(
-            R.drawable.news_images1,
-            R.string.dummy_text
+    companion object {
+        val newsList = listOf(
+            NewsListModel(
+                R.drawable.news_images1,
+                R.string.dummy_text
+            ),
+            NewsListModel(
+                R.drawable.news_images2,
+                R.string.dummy_text
+            ),
+            NewsListModel(
+                R.drawable.news_images3,
+                R.string.dummy_text
+            ),
+            NewsListModel(
+                R.drawable.news_images4,
+                R.string.dummy_text
+            ),
+            NewsListModel(
+                R.drawable.news_images5,
+                R.string.dummy_text
+            ), NewsListModel(
+                R.drawable.news_images3,
+                R.string.dummy_text
+            ), NewsListModel(
+                R.drawable.news_images3,
+                R.string.dummy_text
+            ), NewsListModel(
+                R.drawable.news_images1,
+                R.string.dummy_text
+            )
         )
-    )
+    }
 }
