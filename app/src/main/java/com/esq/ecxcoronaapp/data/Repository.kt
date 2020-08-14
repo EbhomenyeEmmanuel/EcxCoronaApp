9f9e13c7912f8f2b44b06b389273cbbec5dec5d6
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-08-14 01:57:45 +0100

    Removed the home list companion object. Recyclerview not needed here.

--- app/src/main/java/com/esq/ecxcoronaapp/data/Repository.kt
+++ app/src/main/java/com/esq/ecxcoronaapp/data/Repository.kt
@@ -1,57 +1,39 @@
 package com.esq.ecxcoronaapp.data
 
 import com.esq.ecxcoronaapp.R
-import com.esq.ecxcoronaapp.domain.model.HomeListModel
 import com.esq.ecxcoronaapp.domain.model.NewsListModel
 
 class Repository() {
 
-    companion object {
-        val homeInfo = listOf(
-            HomeListModel(
-                R.drawable.card_chart,
-                R.string.how_does_it_spread,
-                R.string.learn_how_covid
-            ),
-            HomeListModel(
-                R.drawable.card_symptoms,
-                R.string.symptoms,
-                R.string.learn_covid_19_symptoms
-            ),
-            HomeListModel(
-                R.drawable.card_prevention,
-                R.string.prevention_and_treatment,
-                R.string.learn_how_to_prevent
-            ),
-            HomeListModel(
-                R.drawable.card_what_to_do,
-                R.string.what_to_do,
-                R.string.learn_what_to_do
-            )
+    val newsList = listOf(
+        NewsListModel(
+            R.drawable.news_images1,
+            R.string.dummy_text
+        ),
+        NewsListModel(
+            R.drawable.news_images2,
+            R.string.dummy_text
+        ),
+        NewsListModel(
+            R.drawable.news_images3,
+            R.string.dummy_text
+        ),
+        NewsListModel(
+            R.drawable.news_images4,
+            R.string.dummy_text
+        ),
+        NewsListModel(
+            R.drawable.news_images5,
+            R.string.dummy_text
+        ), NewsListModel(
+            R.drawable.news_images3,
+            R.string.dummy_text
+        ), NewsListModel(
+            R.drawable.news_images3,
+            R.string.dummy_text
+        ), NewsListModel(
+            R.drawable.news_images1,
+            R.string.dummy_text
         )
-
-        val newsList = listOf(
-            NewsListModel(
-                R.drawable.news_images1,
-                R.string.dummy_text
-            ),
-           NewsListModel(
-                R.drawable.news_images2,
-                R.string.dummy_text
-            ),
-            NewsListModel(
-                R.drawable.news_images3,
-                R.string.dummy_text
-            ),
-            NewsListModel(
-                R.drawable.news_images4,
-                R.string.dummy_text
-            ),
-            NewsListModel(
-                R.drawable.news_images5,
-                R.string.dummy_text
-            )
-        )
-    }
-
+    )
 }
