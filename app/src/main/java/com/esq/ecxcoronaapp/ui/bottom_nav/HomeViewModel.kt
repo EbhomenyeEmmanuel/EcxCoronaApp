3c36261be51738607c908f793959a8f1bd4f4bcb
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-08-14 02:58:15 +0100

    Removed code linked to repo

--- app/src/main/java/com/esq/ecxcoronaapp/ui/bottom_nav/HomeViewModel.kt
+++ app/src/main/java/com/esq/ecxcoronaapp/ui/bottom_nav/HomeViewModel.kt
@@ -1,15 +1,7 @@
 package com.esq.ecxcoronaapp.ui.bottom_nav
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.esq.ecxcoronaapp.data.Repository
-import com.esq.ecxcoronaapp.domain.model.HomeListModel
 
 class HomeViewModel : ViewModel() {
-    private val _homeAdapterData = MutableLiveData<List<HomeListModel>>().apply {
-        value = Repository.homeInfo
-    }
 
-    val homeAdapterData: LiveData<List<HomeListModel>> = _homeAdapterData
 }
